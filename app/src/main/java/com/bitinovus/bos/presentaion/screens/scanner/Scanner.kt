eb5ef5e114d700105a62604e1ad68afd981a153d
redmejia <reynaldoemejia@gmail.com>
2025-03-28 11:57:47 -0700

    Camerax implentation

--- app/src/main/java/com/bitinovus/bos/presentaion/screens/scanner/Scanner.kt
+++ app/src/main/java/com/bitinovus/bos/presentaion/screens/scanner/Scanner.kt
@@ -12,6 +12,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import android.Manifest
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
 fun Scanner() {
@@ -19,6 +28,14 @@ fun Scanner() {
     var isCameraPermissionGranted by remember { mutableStateOf(false) }
     var isCameraPermissionDenied by remember { mutableStateOf(false) }
     val context = LocalContext.current
+    val lifecycle = LocalLifecycleOwner.current
+
+    val cameraController = remember {
+        LifecycleCameraController(context).apply {
+            setEnabledUseCases(CameraController.IMAGE_CAPTURE)
+            bindToLifecycle(lifecycle)
+        }
+    }
 
     val permissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission()
@@ -35,11 +52,33 @@ fun Scanner() {
             ) == PackageManager.PERMISSION_GRANTED -> {
                 isCameraPermissionGranted = true
             }
-
             else -> {
                 permissionLauncher.launch(Manifest.permission.CAMERA)
             }
         }
     }
 
+    if (isCameraPermissionDenied) {
+        Box {
+            Text(text = "Camera need permission to scan")
+        }
+    }
+
+    if (isCameraPermissionGranted) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            AndroidView(
+                modifier = Modifier.fillMaxSize(),
+                factory = {ctx ->
+                    PreviewView(ctx).apply {
+                        scaleType =  PreviewView.ScaleType.FILL_CENTER
+                        implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+                        controller = cameraController
+                    }
+                },
+                onRelease = {
+                    cameraController.unbind()
+                }
+            )
+        }
+    }
 }
