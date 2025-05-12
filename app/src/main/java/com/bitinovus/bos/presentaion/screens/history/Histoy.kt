7075b5163cf6b0320b937baeb71632dea551c94e
redmejia <reynaldoemejia@gmail.com>
2025-05-11 22:44:51 -0700

    strings

--- app/src/main/java/com/bitinovus/bos/presentaion/screens/history/Histoy.kt
+++ app/src/main/java/com/bitinovus/bos/presentaion/screens/history/Histoy.kt
@@ -5,10 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -29,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -91,18 +94,18 @@ fun History(
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp)
-                    .padding(16.dp),
+                    .height(350.dp),
                 shape = RoundedCornerShape(16.dp),
             ) {
                 Column(
                     modifier = Modifier
-                        .padding(6.dp)
+                        .padding(8.dp)
                         .fillMaxSize(),
                     verticalArrangement = Arrangement.SpaceBetween
                 ) {
                     Icon(
                         modifier = Modifier
+                            .size(50.dp)
                             .fillMaxWidth()
                             .align(alignment = Alignment.CenterHorizontally),
                         imageVector = Icons.Default.Warning,
@@ -110,14 +113,14 @@ fun History(
                         tint = PrimaryYellow00
                     )
                     Text(
-                        text = "This is a minimal dialog",
+                        text = stringResource(id = R.string.warning_message),
                         modifier = Modifier
                             .fillMaxWidth(),
                         textAlign = TextAlign.Center,
                     )
                     Row(
                         modifier = Modifier
-                            .padding(4.dp)
+                             .padding(end = 4.dp)
                             .fillMaxWidth(),
                         horizontalArrangement = Arrangement.End,
                         verticalAlignment = Alignment.CenterVertically
@@ -125,12 +128,12 @@ fun History(
                         TextButton(
                             onClick = { historyViewmodel.writeReport() }
                         ) {
-                            Text("Generate", color = PrimaryBlue60)
+                            Text(stringResource(id = R.string.generate), color = PrimaryBlue60)
                         }
                         TextButton(
                             onClick = { isDialogOpen = false }
                         ) {
-                            Text("Cancel", color = PrimaryRed00)
+                            Text(stringResource(id = R.string.cancel), color = PrimaryRed00)
                         }
                     }
                 }
